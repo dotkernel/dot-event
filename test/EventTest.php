5677af62fce164c8ec2424da3b9f8101c22557d1
n3vrax <n3vrax@gmail.com>
2016-09-14 01:23:01 +0300

    test folder

---
+++ test/EventTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright: DotKernel
+ * @library: dotkernel/dot-event
+ * @author: n3vrax
+ * Date: 9/14/2016
+ * Time: 1:21 AM
+ */
+
+namespace DotKernelTest\DotEvent;
+
+/**
+ * Class EventTest
+ * @package DotKernelTest\DotEvent
+ */
+class EventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+
+    }
+}
